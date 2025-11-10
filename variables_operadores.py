393396101e1eb684d63ccf6ca2ccaf79437a34fe
JheysonGalvis <tecnojheyson@gmail.com>
2025-11-10 18:38:47 -0500

    variables y operadores

---
+++ variables_operadores.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+## El número se utiliza para comentar los códigos
+# Las variables no pueden empezar con números
+# Las variables no pueden llamarse con una palabra
+# Predeterminada de python eje: for, is, while
+a = 5 # Tipo de dato entero (int)
+b = 45.3 # Tipo de dato decimal float
+print(b) #Imprimir por terminal el contenido
+print(type(b))  #Imprime el tipo de contenido
+c = input("Ingrese su nombre: ") # Sólo ingresa STR
+d = int(input("Ingrese su edad "))
+e = float(input("Ingrese su estatura "))
+print(c)
+print(d)
+## operadores básicos + - * /
+suma = a + b
+resta  = a - b
+multiplicacion = a * b
+division = a / b
+potencia = a ** 2
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(potencia)
+print(division)
+print("8"*10) #Repetir la cantidad de n de veces el str
+print("hola" + "Pedro")
+#########################################
+print("!! Bienvenido al Cajero Mundial ¡¡")
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+clave = input("Ingrese su clave: ")
+print(f"Su nombre es {nombre} su edad {edad} y la clave {clave}")
+print("Su nombre es ",nombre," su edad es ",edad)
